a672341bb3439abe806d54c9e09216beec3e1841
Asel Atakeev <aselya.atakeeva@gmail.com>
2022-08-24 11:36:09 -0700

    changed name layout to form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./layout/Navbar";
+import Navbar from "./form/Navbar";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddUser from "./users/AddUser";
